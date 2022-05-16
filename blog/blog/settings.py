0bd00d6e13c812e41def6d25270a7f60a5d5cd83
chayanghosh <59326645+chayanghosh@users.noreply.github.com>
2022-05-16 14:17:17 +0530

    Update settings.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -139,4 +139,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'insightexplorerblogs@gmail.com'
-EMAIL_HOST_PASSWORD = 'iexplorer@123'
+EMAIL_HOST_PASSWORD = ''
